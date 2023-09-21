db5fced64db32ef323748834cdc4284c1c820823
ymk <65121726+mikiymk@users.noreply.github.com>
2023-09-21 09:43:32 +0900

    Update fizzbuzz.zig
    doc comment

--- src/fizzbuzz.zig
+++ src/fizzbuzz.zig
@@ -1,18 +1,23 @@
 const std = @import("std");
 
+/// 1からnまでのfizzbuzzを1行ずつ標準出力に書き込みます。
 pub fn fizzbuzz(n: u32) !void {
     const writer = std.io.getStdOut().writer();
 
     for (1..n + 1) |i| {
+        // iを1からnまでループする
         if (i % 15 == 0) {
-            try writer.print("FizzBuzz ", .{});
+            // iが3で割り切れてかつ5で割り切れる場合
+            try writer.print("FizzBuzz\n", .{});
         } else if (i % 5 == 0) {
-            try writer.print("Buzz ", .{});
+            // iが5で割り切れる場合
+            try writer.print("Buzz\n", .{});
         } else if (i % 3 == 0) {
-            try writer.print("Fizz ", .{});
+            // iが3で割り切れる場合
+            try writer.print("Fizz\n", .{});
         } else {
-            try writer.print("{d} ", .{i});
+            // それ以外の場合
+            try writer.print("{d}\n", .{i});
         }
     }
-    try writer.print("\n", .{});
 }
