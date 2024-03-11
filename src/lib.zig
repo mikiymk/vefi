a0e061da738191e1c33b4d43ae1b763dcf49d23a
ymk <65121726+mikiymk@users.noreply.github.com>
2024-03-12 08:47:56 +0900

    Update and rename lib.rs to lib.zig

---
+++ src/lib.zig
@@ -0,0 +1,18 @@
+//! 標準ライブラリっぽいものを実装してみる
+//! 参考
+//! zig https://ziglang.org/documentation/master/std/
+//! java https://docs.oracle.com/javase/jp/21/docs/api/index.html
+//! python https://docs.python.org/ja/3/library/index.html
+//! c++ https://cpprefjp.github.io/reference.html
+//! rust https://doc.rust-lang.org/std/
+//! go https://pkg.go.dev/std
+
+pub const collection = struct {
+  pub const array_list = @import("array_list");
+  pub const linked_list = @import("array_list");
+};
+
+pub const math = struct {
+  pub const complex = @import("complex");
+};
+
