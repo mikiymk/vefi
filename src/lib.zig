e5c510a445d31fe793e799e4e2d5511d783e25b5
ymk <65121726+mikiymk@users.noreply.github.com>
2024-03-12 09:35:47 +0900

    Update lib.zig

--- src/lib.zig
+++ src/lib.zig
@@ -1,4 +1,4 @@
-//! 標準ライブラリっぽいものを実装してみる
+//! 標準ライブラリっぽいものを作ってみる
 //! 参考
 //! zig https://ziglang.org/documentation/master/std/
 //! java https://docs.oracle.com/javase/jp/21/docs/api/index.html
@@ -10,9 +10,13 @@
 pub const collection = struct {
   pub const array_list = @import("array_list");
   pub const linked_list = @import("array_list");
+  pub const doubly_linked_list = @import("array_list");
+  pub const circular_linked_list = @import("array_list");
+  pub const doubly_circular_linked_list = @import("array_list");
 };
 
 pub const math = struct {
   pub const complex = @import("complex");
 };
 
+pub const datetime = @import("array_list");
