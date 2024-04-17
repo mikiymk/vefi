38d605f5d482be5051e6fb7b7c9445495d8eaf39
ymk <65121726+mikiymk@users.noreply.github.com>
2024-04-17 17:42:19 +0900

    Update integer.zig

--- src/primitive/integer.zig
+++ src/primitive/integer.zig
@@ -1,7 +1,26 @@
-pub fn isSignedInt(t: type) bool {}
-pub fn isUnsignedInt(t: type) bool {}
-pub fn isInt(t: type) bool {}
-pub fn bitsOf(t: type) u16 {}
+pub fn isSignedInt(T: type) bool {
+    return switch (@typeinfo(T)) {
+        .Int => |i| i.signed,
+        else => false,
+    };
+}
+
+pub fn isUnsignedInt(T: type) bool {
+    return switch (@typeinfo(T)) {
+        .Int => |i| !i.signed,
+        else => false,
+    };
+}
+
+pub fn isInt(T: type) bool {
+    return @typeinfo(T) == .Int;
+}
+
+pub fn bitsOf(T: type) u16 {
+    assert(T);
+
+    return @typeinfo(T).Int.bits;
+}
 
 pub fn add(a: anytype, b: @TypeOf(a)) @TypeOf(a) {
     assert(isInt(@TypeOf(a)));
