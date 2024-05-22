d0ffb369c31fe19a4a888d41a397167df93990aa
ymk <65121726+mikiymk@users.noreply.github.com>
2024-05-22 18:36:36 +0900

    Create primitive.zig

---
+++ src/primitive.zig
@@ -0,0 +1,7 @@
+//! zigの基本の型と操作
+
+pub const boolean = struct {};
+pub const character = struct {};
+pub const integer = @import("integer.zig");
+pub const float = struct {};
+pub const optional = @import("optional.zig");
