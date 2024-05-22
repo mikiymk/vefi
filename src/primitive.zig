a34ece45dbb860a1994ce960d7fe1f31eaa448e2
ymk <65121726+mikiymk@users.noreply.github.com>
2024-05-22 19:08:51 +0900

    Update primitive.zig

--- src/primitive.zig
+++ src/primitive.zig
@@ -1,7 +1,15 @@
 //! zigの基本の型と操作
 
-pub const boolean = struct {};
-pub const character = struct {};
-pub const integer = @import("integer.zig");
-pub const float = struct {};
-pub const optional = @import("optional.zig");
+pub const boolean = @import("primitive/boolean.zig");
+pub const integer = @import("primitive/integer.zig");
+pub const float = @import("primitive/float.zig");
+
+pub const optional = @import("primitive/optional.zig");
+pub const error_set = @import("primitive/optional.zig");
+pub const error_union = @import("primitive/optional.zig");
+
+pub const array = @import("primitive/optional.zig");
+pub const structure = @import("primitive/optional.zig");
+pub const enumeration = @import("primitive/optional.zig");
+
+pub const types = @import("primitive/optional.zig");
