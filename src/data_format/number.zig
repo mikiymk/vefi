ec31b020173596b4e45d19b686806944a27411b8
ymk <65121726+mikiymk@users.noreply.github.com>
2024-06-15 01:11:08 +0900

    Create number.zig

---
+++ src/data_format/number.zig
@@ -0,0 +1,50 @@
+
+pub const U8 = struct {
+    value: u8,
+
+    pub const size: ?usize = 1;
+
+    pub fn parse(bytes: []u8) struct { @This(), usize } {
+        return .{ .{ value: bytes[0] }, 1 };
+    }
+
+    pub fn serialize(self: @This(), buf: *[]u8) []u8 {
+        buf[0] = self.value;
+
+        return buf[0..1];
+    }
+};
+
+pub const U16be = struct {
+    value: u16,
+
+    pub const size: ?usize = 2;
+
+    pub fn parse(bytes: []u8) struct { @This(), usize } {
+        return .{ .{ value: bytes[0] * 0xff + bytes[1] }, 2 };
+    }
+
+    pub fn serialize(self: @This(), buf: *[]u8) []u8 {
+        buf[0] = self.value / 0xff;
+        buf[1] = self.value % 0xff;
+
+        return buf[0..2];
+    }
+};
+
+pub const U16le = struct {
+    value: u16,
+
+    pub const size: ?usize = 2;
+
+    pub fn parse(bytes: []u8) struct { @This(), usize } {
+        return .{ .{ value: bytes[1] * 0xff + bytes[0] }, 2 };
+    }
+
+    pub fn serialize(self: @This(), buf: *[]u8) []u8 {
+        buf[1] = self.value / 0xff;
+        buf[0] = self.value % 0xff;
+
+        return buf[0..2];
+    }
+};
