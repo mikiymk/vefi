ca4878f46e31ac838734de37fa69beb8a39189c9
ymk <65121726+mikiymk@users.noreply.github.com>
2024-06-15 00:18:57 +0900

    Update data_format.zig

--- src/data_format.zig
+++ src/data_format.zig
@@ -5,13 +5,13 @@ const lib = @import("./lib.zig");
 
 // バイナリファイル
 
-pub const gif = @import("file_format/gif.zig");
-pub const jpeg = @import("file_format/jpeg.zig");
-pub const png = @import("file_format/png.zig");
+pub const gif = @import("data_format/gif.zig");
+pub const jpeg = @import("data_format/jpeg.zig");
+pub const png = @import("data_format/png.zig");
 
 // テキストファイル
 
-pub const json = @import("file_format/json.zig");
+pub const json = @import("data_format/json.zig");
 
 test {
     std.testing.refAllDecls(@This());
