a15f0b202ae02e76c9f3e4377d2b2c13be93e9c5
ymk <65121726+mikiymk@users.noreply.github.com>
2024-06-15 17:13:49 +0900

    Update gif.zig

--- src/data_format/gif.zig
+++ src/data_format/gif.zig
@@ -25,13 +25,17 @@ pub const LogicalScreenDescriptor = utils.Block(.{
 });
 
 pub const ImageDescriptor = utils.Block(.{
-    .image_separator = number.Fixed(@as(u8, 0x2C)),
+    .image_separator = number.Fixed(number.U8, 0x2C),
     .image_left_position = number.U16le,
     .image_top_position = number.U16le,
     .image_width = number.U16le,
     .image_height = number.U16le,
     .pack = utils.Pack(1, .{
-        
+        .local_color_table_flag = bool,
+        .interlace_flag = bool,
+        .sort_flag = bool,
+        .reserved = u2,
+        .size_of_local_color_table = u3,
     }),
 });
 
@@ -42,3 +46,47 @@ pub const Color = utils.Block(.{
 });
 
 pub const ColorTable = utils.SizedArray(Color);
+
+pub const DataSubBlock = utils.SizedArray(number.U8);
+pub const BlockTerminator = number.Fixed(number.U8, 0x00);
+pub const SubBlocks = utils.TermArray(DataSubBlock, BlockTerminator);
+
+pub const ImageData = utils.Block(.{
+    .lzw_minimum_color_size = number.U8,
+    .image_data = SubBlocks,
+});
+
+pub const GraphicControlExtention = utils.Block(.{
+    .extention_introducer = number.Fixed(number.U8, 0x21),
+    .graphic_control_label = number.Fixed(number.U8, 0xF9),
+
+    .block_size = number.Fixed(number.U8, 0x04),
+    .pack = utils.Pack(.{
+        .reserved = u3,
+        .disposal_method = enum(u3) {
+            .no_disposal = 0,
+            .do_not_disposal = 1,
+            .restore_to_background = 2,
+            .restore_to_previous = 3,
+        },
+        .user_input_flag = bool,
+        .transparent_color_flag = bool,
+    }),
+    .delay_time = number.U16le,
+    .transparent_color_index = number.U8,
+
+    .block_terminator = BlockTerminator,
+});
+
+pub const CommentExtention = utils.Block(.{
+    .extention_introducer = number.Fixed(number.U8, 0x21),
+    .comment_label = number.Fixed(number.U8, 0xFE),
+    .comment_data = SubBlocks,
+});
+
+pub const PlainTextExtention = utils.Block(.{
+    .extention_introducer = number.Fixed(number.U8, 0x21),
+    .plain_text_label = number.Fixed(number.U8, 0x01),
+
+    .block_size = number.U8,
+});
