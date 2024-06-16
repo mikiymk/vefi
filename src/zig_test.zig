62dcf11ff4b14100072c6517c87a7c4091be338a
ymk <65121726+mikiymk@users.noreply.github.com>
2024-06-17 01:45:20 +0900

    Create zig_test.zig

---
+++ src/zig_test.zig
@@ -0,0 +1,53 @@
+//! lib.zig_test
+//!
+//! Zig言語の基本の書き方を確認する。
+
+test "整数リテラル 10進数" {}
+test "整数リテラル 2進数" {}
+test "整数リテラル 8進数" {}
+test "整数リテラル 16進数" {}
+
+test "小数リテラル 10進数" {}
+test "小数リテラル 16進数" {}
+
+test "文字リテラル" {}
+test "文字列リテラル" {}
+test "文字列リテラル 複数行" {}
+
+test "整数型" {}
+
+test "浮動小数点数型" {}
+
+test "comptime_int" {}
+
+test "comptime_float" {}
+
+test "bool" {}
+
+test "void" {}
+
+test "C互換数値型" {}
+
+test "配列" {}
+test "配列 番兵つき" {}
+
+test "ベクトル型" {}
+
+test "ポインタ型 単要素ポインタ" {}
+test "ポインタ型 複数要素ポインタ" {}
+
+test "スライス型" {}
+
+test "構造体型" {}
+
+test "列挙型" {}
+test "合同型" {}
+
+test "不透明型" {}
+
+test "制御構造 ブロック" {}
+test "制御構造 if" {}
+test "制御構造 switch" {}
+test "制御構造 for" {}
+test "制御構造 while" {}
+test "制御構造 defer" {}
