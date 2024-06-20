ef1c68296c8dbd0a21f294c0feb935f30923c9d0
ymk <65121726+mikiymk@users.noreply.github.com>
2024-06-20 11:36:47 +0900

    Update root.zig

--- src/root.zig
+++ src/root.zig
@@ -30,17 +30,19 @@ pub const string = @import("string.zig");
 pub const time = @import("time.zig");
 
 pub const input_output = @import("input_output.zig");
+pub const io = input_output;
 pub const file_system = struct {};
+pub const fs = file_system;
 pub const network = struct {};
 pub const memory = struct {};
 
 pub const result = @import("result.zig");
 pub const regular_expression = @import("regular_expression.zig");
-pub const graphic = struct {};
 pub const random = @import("random.zig");
 pub const iterator = @import("iterator.zig");
-pub const locale = struct {};
 
+pub const graphic = struct {};
+pub const locale = struct {};
 pub const data_format = @import("data_format.zig");
 pub const language = struct {
     // 自然言語
