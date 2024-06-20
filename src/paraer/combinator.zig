75731d010ee785a166f3c015bb408532935777a9
ymk <65121726+mikiymk@users.noreply.github.com>
2024-06-20 17:27:44 +0900

    Update combinator.zig

--- src/paraer/combinator.zig
+++ src/paraer/combinator.zig
@@ -1,11 +1,14 @@
 pub const ParseResult(Parser: type) type {
-    return struct { Parser.ValueType, usize };
+    return error{ParseError}!struct { Parser.ValueType, usize };
 }
 
 pub fn U8() type {
     return struct {
         pub const ValueType = u8;
         pub fn parse(bytes: []const u8) ParseResult(@This()) {
+            if (bytes.len < 1) {
+                return error.ParseError;
+            }
             return .{ bytes[0], 1 };
         }
     };
