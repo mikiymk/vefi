14713a7b58f36f174ed01301fe79c0e4256e7931
ymk <65121726+mikiymk@users.noreply.github.com>
2024-06-20 12:05:08 +0900

    Update string.zig

--- src/string.zig
+++ src/string.zig
@@ -1,8 +1,21 @@
 const std = @import("std");
 const lib = @import("root.zig");
 
-pub const utf8_string = struct {};
+/// java: https://docs.oracle.com/javase/jp/8/docs/technotes/guides/intl/encoding.doc.html
+/// javascript: https://encoding.spec.whatwg.org/#names-and-labels
+/// javascript: https://developer.mozilla.org/docs/Web/API/Encoding_API/Encodings
+/// python: https://docs.python.org/3/library/codecs.html#standard-encodings
+pub const Encoding = enum {
+    us_ascii,
+    utf8,
+    utf16,
+    utf16le,
+    utf16be,
+    utf32,
+};
+
 pub const ascii_string = struct {};
+pub const utf8_string = struct {};
 
 test {
     std.testing.refAllDecls(@This());
