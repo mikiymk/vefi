ce0f70b34f8102aeb22cefa74eb1391cbf420447
ymk <65121726+mikiymk@users.noreply.github.com>
2024-06-29 10:40:29 +0900

    Update dynamic_array.zig

--- src/collection/dynamic_array.zig
+++ src/collection/dynamic_array.zig
@@ -14,6 +14,7 @@ pub fn DynamicArray(T: type) type {
         value: []T,
         size: usize,
 
+        /// 配列を空の状態で初期化する。
         pub fn init() @This() {
             return .{
                 .value = &[_]T{},
@@ -21,10 +22,13 @@ pub fn DynamicArray(T: type) type {
             };
         }
 
+        /// メモリを解放する。
         pub fn deinit(self: *@This(), allocator: Allocator) void {
             allocator.free(self.value);
         }
 
+        /// 値を配列の最も後ろに追加する。
+        /// 配列の長さが足りないときは拡張した長さの配列を再確保する。
         pub fn push(self: *@This(), allocator: Allocator, item: T) AllocatorError!void {
             if (self.value.len <= self.size) {
                 try self.extendSize(allocator);
@@ -34,9 +38,17 @@ pub fn DynamicArray(T: type) type {
             self.size += 1;
         }
 
-        pub fn pop() void {}
+        /// 配列の最も後ろの要素を削除し、その値を返す。
+        /// 配列が要素を持たない場合、配列を変化させずにnullを返す。
+        pub fn pop(self: *@This()) ?T {
+            if (self.size == 0) return null;
+            self.size -= 1;
+            return self.value[self.size];
+        }
 
-        pub fn get(n: usize) *T {
+        /// 配列の`N`番目の要素を返す。
+        /// Nが配列の長さより大きい場合、nullを返す。
+        pub fn get(n: usize) ?*T {
             _ = n;
         }
 
@@ -59,6 +71,10 @@ pub fn DynamicArray(T: type) type {
             _ = n;
         }
 
+        pub fn asSlice(self: @This()) []const T {
+            return self.value[0..self.size];
+        }
+
         pub fn copyToSlice(self: @This(), allocator: Allocator) AllocatorError![]const T {
             var slice = try allocator.alloc(T, self.size);
             @memcpy(slice[0..self.size], self.value[0..self.size]);
@@ -73,16 +89,24 @@ pub fn DynamicArray(T: type) type {
             return slice;
         }
 
+        /// メモリを再確保して配列の長さを拡張する。
         fn extendSize(self: *@This(), allocator: Allocator) AllocatorError!void {
-            const new_length = if (self.size == 0) 5 else self.size * 2;
+            const initial_size = 8;
+            const extend_factor = 2;
+
+            const old_length = self.value.len;
+            const new_length = if (old_length == 0)
+                initial_size
+            else
+                old_length * extend_factor;
 
             var old_values = self.value;
             var new_values = try allocator.alloc(T, new_length);
 
             @memcpy(new_values[0..self.size], old_values[0..self.size]);
 
-            self.value = new_values;
             allocator.free(old_values);
+            self.value = new_values;
         }
     };
 }
