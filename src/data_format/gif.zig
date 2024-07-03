139538f5739d26a303824ee813b1f8ffbae883ec
ymk <65121726+mikiymk@users.noreply.github.com>
2024-07-03 13:47:19 +0900

    Update gif.zig

--- src/data_format/gif.zig
+++ src/data_format/gif.zig
@@ -12,17 +12,19 @@ const number = lib.data_format.number;
 const string = lib.data_format.string;
 const utils = lib.data_format.utils;
 
-const parser = lib.parser.combinator;
-const Result = parser.Result;
+const p = lib.parser.combinator;
+const Result = p.Result;
 const Allocator = lib.allocator.Allocator;
 
 pub const Gif = struct {
     header: Header,
+    logical_screen_descriptor: LogicalScreenDescriptor,
 };
 
-pub fn parseGif(allocator: Allocator, input: []const u8) Result(Gif, error{}) {
-    return parser.block(Gif, &.{
-        .{ "header", parseHeader },
+pub fn gif(allocator: Allocator, input: []const u8) Result(Gif, error{}) {
+    return p.block(Gif, &.{
+        .{ "header", header },
+        .{ "logical_screen_descriptor", logicalScreenDescriptor },
     }).parse(allocator, input);
 }
 
@@ -31,27 +33,39 @@ pub const Header = struct {
     version: [3]u8,
 };
 
-pub fn parseHeader(allocator: Allocator, input: []const u8) Result(Header, error{}) {
-    return parser.block(Header, &.{
-        .{ "signature", parser.arrayFixed(3, parser.byte) },
-        .{ "version", parser.arrayFixed(3, parser.byte) },
+pub fn header(allocator: Allocator, input: []const u8) Result(Header, error{}) {
+    return p.block(Header, &.{
+        .{ "signature", p.arrayFixed(3, p.byte) },
+        .{ "version", p.arrayFixed(3, p.byte) },
     }).parse(allocator, input);
 }
 
-pub const LogicalScreenDescriptor = utils.Block(.{
-    .logical_screen_width = number.u16_le,
-    .logical_screen_height = number.u16_le,
+pub const LogicalScreenDescriptor = struct {
+    logical_screen_width: u16,
+    logical_screen_height: u16,
 
-    .pack = utils.Pack(1, .{
-        .global_color_table_flag = bool,
-        .color_resolution = u3,
-        .sort_flag = bool,
-        .size_of_global_color_table = u3,
-    }),
+    global_color_table_flag = bool,
+    color_resolution = u3,
+    sort_flag = bool,
+    size_of_global_color_table = u3,
 
-    .background_color_index = number.byte,
-    .aspect_ratio = number.byte,
-});
+    background_color_index = u8,
+    aspect_ratio = u8,
+};
+pub fn logicalScreenDescriptor(allocator: Allocator, input: []const u8) Result(LogicalScreenDescriptor, error{}) {
+    return p.block(Header, &.{
+        .{ "logical_screen_width", p.u16(.little) },
+        .{ "logical_screen_height", p.u16(.little) },
+        p.bitsSubBlock(p.byte, &.{
+            .{ "global_color_table_flag", bool },
+            .{ "color_resolution", u3 },
+            .{ "sort_flag", bool },
+            .{ "size_of_global_color_table", u3 },
+        }),
+        .{ "background_color_index", p.byte },
+        .{ "aspect_ratio", p.byte },
+    }).parse(allocator, input);
+}
 
 pub const ImageDescriptor = utils.Block(.{
     .image_separator = number.Fixed(number.byte, 0x2C),
