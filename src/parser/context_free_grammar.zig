4dba276d8636947aee631656180ffe7f4ab0370c
ymk <65121726+mikiymk@users.noreply.github.com>
2024-07-19 02:18:23 +0900

    Update context_free_grammar.zig

--- src/parser/context_free_grammar.zig
+++ src/parser/context_free_grammar.zig
@@ -5,12 +5,18 @@ test {
     std.testing.refAllDecls(@This());
 }
 
+/// 文法。
+/// 生成規則の集まりでできている。
+/// 最初の規則の左辺に登場する非終端記号を開始記号とする。
 pub fn Grammar(Term: type, NonTerm: type) type {
     return struct {
         rules: []const ProductionRule(Term, NonTerm),
     };
 }
 
+/// 生成規則。
+/// 左辺に1個の非終端記号を持つ。
+/// 右辺に0個以上の終端記号または非終端記号を持つ。
 pub fn ProductionRule(Term: type, NonTerm: type) type {
     return struct {
         left: NonTerm,
@@ -18,6 +24,7 @@ pub fn ProductionRule(Term: type, NonTerm: type) type {
     };
 }
 
+/// 終端記号または非終端記号。
 pub fn Symbol(Term: type, NonTerm: type) type {
     return union(enum) {
         term: Term,
