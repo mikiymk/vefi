fa5b96437e8f92a5227e773402598796e1246f38
ymk <65121726+mikiymk@users.noreply.github.com>
2025-01-21 12:46:46 +0900

    Update array-static.zig

--- src/collection/array-static.zig
+++ src/collection/array-static.zig
@@ -1,16 +1,41 @@
 const std = @import("std");
 const lib = @import("../root.zig");
 
-const Allocator = lib.allocator.Allocator;
-const AllocatorError = lib.allocator.AllocatorError;
-
 const StaticArrayOptions = struct {
 };
 
 /// 静的配列 (Static Array)
 pub fn StaticArray(T: type, size: usize, comptime options: StaticArrayOptions) type {
     return struct {
-        value: [size]T,
+        values: [size]T,
+
+pub fn init() @This() {}
+
+pub fn deinit(self: @This()) void {}
+
+fn checkBound(self: @This(), index: usize) bool {
+    return index < self.values.len;
+}
+
+pub fn getUnsafe(self: @This(), index: usize) T {
+    assert(self.checkBound(index));
+    return self.values[index];
+}
+
+pub fn get(self: @This(), index: usize) Error!T {
+    if (self.checkBound(index)) return error.OutOfIndex;
+    return self.getUnsafe(index);
+}
+
+pub fn setUnsafe(self: *@This(), index: usize, value: T) void {
+    assert(self.checkBound(index));
+    self.values[index] = value;
+}
+
+pub fn set(self: *@This(), index: usize, value: T) Error!T {
+    if (self.checkBound(index)) return error.OutOfIndex;
+    return self.setUnsafe(index, value);
+}
     };
 }
 
