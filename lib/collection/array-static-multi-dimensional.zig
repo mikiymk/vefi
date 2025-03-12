7a6388a0860786270c069b3a9f80ed4546bf4219
ymk <65121726+mikiymk@users.noreply.github.com>
2025-03-12 12:16:38 +0900

    Update array-static-multi-dimensional.zig

--- lib/collection/array-static-multi-dimensional.zig
+++ lib/collection/array-static-multi-dimensional.zig
@@ -1,12 +1,10 @@
 pub const StaticMultiDimensionalArrayOptions = struct {};
 
 /// 静的多次元配列 (Static Multi-Dimensional Array)
-pub fn StaticMultiDimensionalArray(T: type, comptime dimension: usize, sizes: [dimension]usize, comptime options: StaticMultiDimensionalArrayOptions) type {
-    _ = options;
-
+pub fn StaticMultiDimensionalArray(T: type, comptime dimension: usize, sizes: [dimension]usize) type {
     return struct {
         pub const Indexes = [dimension]usize;
-        pub const length: usize = blk: {
+        const length: usize = blk: {
             var l: usize = 1;
             for (sizes) |size| {
                 l *= size;
@@ -14,5 +12,17 @@ pub fn StaticMultiDimensionalArray(T: type, comptime dimension: usize, sizes: [d
             break :blk l;
         };
         value: [length]T,
+
+         fn getRawIndex(indexes: Indexes) usize {
+            var index: usize = 0;
+
+            for (sizes, indexes) |size, i| {
+                 index = index * size + i;
+            }
+
+             return index; 
+        }
+
+        pub fn size (self: @This())usize { return length; }
     };
 }
