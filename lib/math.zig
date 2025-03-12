4ec2d54a86aa85e64b023ac948e125ae21b51518
mikiymk <mikiymk001@gmail.com>
2025-03-12 17:13:33 +0900

    update math

--- lib/math.zig
+++ lib/math.zig
@@ -40,6 +40,10 @@ pub fn absDiff(left: anytype, right: @TypeOf(left)) lib.types.Integer.Unsigned(@
 // https://docs.oracle.com/javase/jp/23/docs/api/java.base/java/lang/Math.html
 // https://docs.python.org/ja/3.13/library/math.html
 
+pub const e: comptime_float = 0.0;
+pub const pi: comptime_float = 0.0;
+pub const tau: comptime_float = 0.0;
+
 pub fn sin() void {}
 pub fn cos() void {}
 pub fn tan() void {}
@@ -82,3 +86,70 @@ pub fn erf() void {}
 pub fn erfc() void {}
 pub fn tgamma() void {}
 pub fn lgamma() void {}
+
+pub fn assoc_laguerre() void {}
+pub fn assoc_legendre() void {}
+pub fn beta() void {}
+pub fn comp_ellint_1() void {}
+pub fn comp_ellint_2() void {}
+pub fn comp_ellint_3() void {}
+pub fn cyl_bessel_i() void {}
+pub fn cyl_bessel_j() void {}
+pub fn cyl_bessel_k() void {}
+pub fn cyl_neumann() void {}
+pub fn ellint_1() void {}
+pub fn ellint_2() void {}
+pub fn ellint_3() void {}
+pub fn expint() void {}
+pub fn hermite() void {}
+pub fn laguerre() void {}
+pub fn legendre() void {}
+pub fn riemann_zeta() void {}
+pub fn sph_bessel() void {}
+pub fn sph_legendre() void {}
+pub fn sph_neumann() void {}
+
+pub fn ceil() void {}
+pub fn floor() void {}
+pub fn round() void {}
+pub fn trunc() void {}
+pub fn rint() void {}
+
+pub fn mod() void {}
+pub fn rem() void {}
+
+pub fn max() void {}
+pub fn min() void {}
+pub fn dim() void {}
+
+pub fn fma() void {}
+
+pub fn lerp() void {}
+
+pub fn fpclassify() void {}
+pub fn isfinite() void {}
+pub fn isinf() void {}
+pub fn isnan() void {}
+pub fn isnormal() void {}
+pub fn signbit() void {}
+
+pub fn isgreater() void {}
+pub fn isgreaterequal() void {}
+pub fn isless() void {}
+pub fn islessequal() void {}
+pub fn islessgreater() void {}
+pub fn isunordered() void {}
+
+pub fn clamp() void {}
+pub fn copySign() void {}
+pub fn nextAfter() void {}
+pub fn nextUp() void {}
+pub fn nextDown() void {}
+pub fn signum() void {}
+pub fn ulp() void {}
+
+pub fn comb() void {}
+pub fn factorial() void {}
+pub fn gcd() void {}
+pub fn lcm() void {}
+pub fn perm() void {}
