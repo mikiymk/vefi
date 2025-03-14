2c7e3255a7bf2975bac1afe0e121a4af8e58ab2d
ymk <65121726+mikiymk@users.noreply.github.com>
2025-03-14 18:51:26 +0900

    Update array.zig

---
+++ lib/collection/array.zig
@@ -0,0 +1,32 @@
+
+pub const StaticArray = @import("./array/static.zig").StaticArray;
+pub const DynamicArray = @import("./array/dynamic.zig").DynamicArray;
+pub const StaticMultiDimensionalArray = @import("./array/static-multi-dimensional.zig").StaticMultiDimensionalArray;
+pub const StringArray = @import("./array/string.zig");
+pub const bit_array = struct {};
+
+pub fn isArray(T: type) bool {
+    const match = lib.interface.match(T);
+
+    return match.hasFn("get") and
+        match.hasFn("set") and
+        match.hasFn("size");
+}
+
+pub fn isDynamicArray(T: type) bool {
+    const match = lib.interface.match(T);
+
+    return isArray(T) and
+        match.hasFn("pushFront") and
+        match.hasFn("pushBack") and
+        match.hasFn("popFront") and
+        match.hasFn("popBack");
+}
+
+test "array is array" {
+    const expect = lib.assert.expect;
+
+    try expect(isArray(StaticArray(u8, 5)));
+    try expect(isDynamicArray(DynamicArray(u8)));
+    try expect(isDynamicArray(StringArray));
+}
