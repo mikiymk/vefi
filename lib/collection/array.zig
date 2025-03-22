55581840224cca084179258ba314895c58c8ff9a
ymk <65121726+mikiymk@users.noreply.github.com>
2025-03-23 08:46:55 +0900

    Update array.zig

--- lib/collection/array.zig
+++ lib/collection/array.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const lib = @import("../root.zig");
 
 pub const StaticArray = @import("array/StaticArray.zig").StaticArray;
+pub const StaticDynamicArray = @import("array/StaticDynamicArray.zig").StaticDynamicArray;
 pub const DynamicArray = @import("array/DynamicArray.zig").DynamicArray;
 pub const StaticMultiDimensionalArray = @import("array/StaticMultiDimensionalArray.zig").StaticMultiDimensionalArray;
 pub const StringArray = @import("array/StringArray.zig");
