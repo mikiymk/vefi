bb448719e4b2566c8a3eecdb2edd2e0320f3e3f2
ymk <65121726+mikiymk@users.noreply.github.com>
2025-03-24 13:24:09 +0900

    Update circular.zig

--- lib/collection/list/node/circular.zig
+++ lib/collection/list/node/circular.zig
@@ -1,1 +1,13 @@
 
+pub fn size(head: anytype) usize {
+    const head_ = head orelse return 0;
+    var node = head_.next;
+    var count: usize = 0;
+
+    while (true) : (node = node.next) {
+        count += 1;
+        if (node.next == head) break;
+    }
+
+    return count;
+}
