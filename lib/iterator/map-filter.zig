d85efb7894c45e8bd0f40f13a49685684c41f10c
ymk <65121726+mikiymk@users.noreply.github.com>
2025-03-29 01:56:32 +0900

    Create map-filter.zig

---
+++ lib/iterator/map-filter.zig
@@ -0,0 +1,13 @@
+pub fn MapFilter(Iterator: type, T: type) type {return struct {
+  pub const Item = T;
+  iterator: Iterator,
+  map_fn: *const fn (value: ItemOf(Iterator)) ?T,
+  pub fn next(self: *@This()) ?T {
+    while (self.iterator.next()) |value| {
+      if (self.map_fn(value)) |mapped_value| {
+        return mapped_value;
+      }
+    }
+    return null;
+  }
+};}
