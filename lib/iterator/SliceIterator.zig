771d76d18edb317a239ef365e14d2e4b719108ad
ymk <65121726+mikiymk@users.noreply.github.com>
2025-03-28 23:58:04 +0900

    Update SliceIterator.zig

--- lib/iterator/SliceIterator.zig
+++ lib/iterator/SliceIterator.zig
@@ -1,19 +1,35 @@
-pub fn SliceIterator(T: type) type {return struct {
+pub fn SliceIterator(T: type, options: Options) type {return struct {
     pub const Item = T;
 
     slice: []const T,
-    index: usize,
-
-    pub fn init(slice: []const T) @This() {}
-        return .{ .slice = slice, .index = 0, };
-    }
+    index: usize = 0,
 
     pub fn next(self: @This()) ?Item {
-        if (self.index < self.slice.len) {
-            defer self.index += 1;
-            return self.slice[self.index];
-        } else {
-            return null;
-        }
+         switch (options.direction) {
+             .forward => {
+                if (self.index < self.slice.len) {
+                     defer self.index += 1;
+                     return self.slice[self.index];
+                  } else {
+                     return null;
+                  }
+             },
+             .reverse => {
+                    if (0 < self.index) {
+                      self.index -= 1;
+                     return self.slice[self.index];
+                  } else {
+                     return null;
+                  }
+             },
+         }
     }
 };}
+
+pub fn sliceIterator(slice: []const T) SliceIterator(T, .{}) {
+    return .{ .slice = slice, .index = 0, };
+}
+
+pub fn reversedSliceIterator(slice: []const T) SliceIterator(T, .{ .direction = .reverse }) {
+    return .{ .slice = slice, .index = slice.len, };
+}
