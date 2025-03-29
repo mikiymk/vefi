97d83aeeff8a86491834c0dafd572d5a3986baf6
ymk <65121726+mikiymk@users.noreply.github.com>
2025-03-29 16:37:39 +0900

    Update consumer.zig

--- lib/iterator/consumer.zig
+++ lib/iterator/consumer.zig
@@ -9,3 +9,12 @@ pub fn reduce(
   }
   return acc;
 }
+
+pub fn every(iterator: anytype) bool {
+  while (iterator.next()) |value| {
+    if (!value) {
+      return false;
+    }
+  }
+  return true;
+}
