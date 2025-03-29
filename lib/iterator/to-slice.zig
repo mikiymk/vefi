073ab1cfcc267e3e22e039e4c77da58b7494344d
ymk <65121726+mikiymk@users.noreply.github.com>
2025-03-29 16:39:41 +0900

    Delete lib/iterator/to-slice.zig

--- lib/iterator/to-slice.zig
+++
@@ -1,8 +0,0 @@
-pub fn toSlice(iterator: anytype, allocator: Allocator) []ItemOf(@TypeOf(iterator)) {
-  const array = DynamicArray(@TypeOf(iterator).Item).init();
-  defer array.deinit();
-  while (iterator.next()) |value| {
-    areay.push(allocator, value);
-  }
-  return array.copyToSlice(allocator);
-}
