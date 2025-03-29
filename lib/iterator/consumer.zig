eb73c6137feeb0141feab1508f2202b515c8811d
ymk <65121726+mikiymk@users.noreply.github.com>
2025-03-29 16:38:55 +0900

    Update consumer.zig

--- lib/iterator/consumer.zig
+++ lib/iterator/consumer.zig
@@ -27,3 +27,12 @@ pub fn some(iterator: anytype) bool {
   }
   return false;
 }
+
+pub fn toSlice(iterator: anytype, allocator: Allocator) []ItemOf(@TypeOf(iterator)) {
+  const array = DynamicArray(@TypeOf(iterator).Item).init();
+  defer array.deinit();
+  while (iterator.next()) |value| {
+    areay.push(allocator, value);
+  }
+  return array.copyToSlice(allocator);
+}
