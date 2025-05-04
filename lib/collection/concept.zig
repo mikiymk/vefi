9a6242e7f89d9179e3d8093d382fb5d740897141
ymk <65121726+mikiymk@users.noreply.github.com>
2025-05-04 21:00:28 +0900

    Update concept.zig

--- lib/collection/concept.zig
+++ lib/collection/concept.zig
@@ -1,4 +1,14 @@
+/// 型がコレクションの概念に合致するかを判定する。
 pub fn collection(T: type) bool {
+ const match = Match.init(T);
+
+ return match.hasDecl("Item") and
+  match.decl("Item").is(type) and
+  match.hasDecl("size");
+}
+
+/// 型がランダムアクセス可能かを判定する。
+pub fn randomAccess(T: type) bool {
  const match = Match.init(T);
 
  return Bool.all(&.{
