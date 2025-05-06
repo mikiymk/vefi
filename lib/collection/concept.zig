989a6d262ed050b2a7f4a619cae7c2342e5a4310
ymk <65121726+mikiymk@users.noreply.github.com>
2025-05-07 02:26:31 +0900

    Update concept.zig

--- lib/collection/concept.zig
+++ lib/collection/concept.zig
@@ -8,12 +8,10 @@ pub fn collection(T: type) bool {
   t.decl("size").returns(usize);
 }
 
-/// 型がランダムアクセス可能かを判定する。
-pub fn randomAccess(T: type) bool {
+/// 型がインデックスでアクセス可能かを判定する。
+pub fn indexed(T: type) bool {
  const match = Match.init(T);
 
- return t.hasDecl("Item") and
-  t.decl("Item").is(type) and
-  t.hasMethod("size") and
-  t.decl("size").returns(usize);
+ return t.hasMethod("get") and
+  t.hasMethod("set");
 }
