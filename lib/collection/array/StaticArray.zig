0a92a3d6c662f65648f5b332b73a8841b704bb0f
ymk <65121726+mikiymk@users.noreply.github.com>
2025-06-19 11:12:10 +0900

    Update StaticArray.zig

--- lib/collection/array/StaticArray.zig
+++ lib/collection/array/StaticArray.zig
@@ -13,21 +13,9 @@ pub fn StaticArray(T: type, array_size: usize) type {
 
         values: [array_size]T,
 
-        /// 配列を初期化する。
-        /// 与えた配列の値で初期化する。
-        pub fn init(initial_array: [array_size]T) @This() {
-            return .{ .values = initial_array };
-        }
-
-        /// 配列を初期化する。
-        /// 配列をすべて同じ値に初期化する。
-        pub fn initWithValue(initial_value: T) @This() {
-            return init(.{initial_value} ** array_size);
-        }
-
-        /// 配列を解放する。
-        pub fn deinit(self: *@This()) void {
-            self.* = undefined;
+        /// 値を初期化せずに配列を作成する。
+        pub fn init() @This() {
+            return .{ .values = undefined };
         }
 
         /// インデックスが配列の範囲内かどうか判定する。
@@ -35,16 +23,6 @@ pub fn StaticArray(T: type, array_size: usize) type {
             const size_ = self.size();
 
             return 0 <= index and index < size_;
-        }
-
-        /// インデックス範囲が配列の範囲内かどうか判定する。
-        pub fn isInBoundRange(self: @This(), range: Range) bool {
-            const begin, const end = range;
-            const size_ = self.size();
-
-            return 0 <= begin and begin < size_ and
-                0 < end and end <= size_ and
-                begin < end;
         }
 
         /// 配列の要素数を返す。
@@ -68,15 +46,6 @@ pub fn StaticArray(T: type, array_size: usize) type {
             return &self.values[index];
         }
 
-        /// 配列の範囲の要素のスライスを返す。
-        /// 配列の範囲外の場合、`null`を返す。
-        pub fn slice(self: @This(), range: Range) ?[]const T {
-            if (!self.isInBoundRange(range)) return null;
-            const begin, const end = range;
-
-            return self.values[begin..end];
-        }
-
         /// 配列の`index`番目の要素の値を設定する。
         /// `index`が配列の範囲外の場合、エラーを返す。
         pub fn set(self: *@This(), index: usize, value: T) IndexError!void {
@@ -85,39 +54,25 @@ pub fn StaticArray(T: type, array_size: usize) type {
             self.values[index] = value;
         }
 
-        /// 配列の`index`番目から先を新しい値のスライスで更新する。
-        /// `index`からスライスの範囲が配列の範囲外の場合、エラーを返す。
-        pub fn setAll(self: *@This(), index: usize, values: []const T) IndexError!void {
-            if (!self.isInBound(index)) return error.OutOfBounds;
+        /// 配列の繰り返しオブジェクト
+        pub const Iterator = struct {
+            pub const Item = T;
+            index: usize = 0,
+            ref: Array,
 
-            @memcpy(self.values[index..][0..values.len], values);
-        }
-
-        /// 配列の`begin`番目(含む)から`end`番目(含まない)の要素の値をまとめて設定する。
-        /// `index`が配列の範囲外の場合、エラーを返す。
-        pub fn setFill(self: *@This(), range: Range, value: T) IndexError!void {
-            if (!self.isInBoundRange(range)) return error.OutOfBounds;
-            const begin, const end = range;
-
-            @memset(self.values[begin..end], value);
-        }
-
-        /// 配列の`left`番目と`right`番目の要素の値を交換する。
-        /// `left`か`right`が配列の範囲外の場合、エラーを返す。
-        pub fn swap(self: *@This(), left: usize, right: usize) IndexError!void {
-            if (!self.isInBound(left)) return error.OutOfBounds;
-            if (!self.isInBound(right)) return error.OutOfBounds;
-
-            const tmp = self.get(left).?;
-            self.set(left, self.get(right).?) catch unreachable;
-            self.set(right, tmp) catch unreachable;
-        }
-
-        /// 配列の要素の並びを逆転する。
-        pub fn reverse(self: *@This()) void {
-            for (0..(self.size() / 2)) |i| {
-                self.swap(i, self.size() - i - 1) catch unreachable;
+            /// あれば次の値を返す
+            pub fn next(self: *@This()) ?T {
+                if (self.ref.isInBound(self.index)) {
+                    defer self.index += 1;
+                    return self.ref.values[self.index];
+                }
+                return null;
             }
+        };
+
+        /// 繰り返しオブジェクトを作成する
+        pub iterator(self: *@This()) Iterator {
+            return .{ .ref = self };
         }
 
         /// 配列を文字列にする。
